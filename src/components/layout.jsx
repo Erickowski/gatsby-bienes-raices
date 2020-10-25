4165c355d9e5f60315491f1236729cd7c8cd723f
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-25 12:31:39 -0600

    Configurando React Helmet

---
+++ src/components/layout.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Helmet from "react-helmet"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Helmet>
+        <title>Bienes Raices Gatsby</title>
+        <meta
+          name="description"
+          content="Sitio web de bienes raices en Gatsby"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossOrigin="anonymous"
+        />
+      </Helmet>
+      {children}
+    </>
+  )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Layout from "../components/layout"
 
 const Index = () => {
-  return <h1>Hola mundo</h1>
+  return (
+    <Layout>
+      <h1>Hola mundo</h1>
+    </Layout>
+  )
 }
 
 export default Index
