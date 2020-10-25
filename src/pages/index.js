f2e0a9c64afacbe342b05131a7992fe1f3d9fc10
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-25 14:54:15 -0600

    Obteniendo informacion desde strapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
 
+import useInicio from "../hooks/useInicio"
+
 const Index = () => {
+  const resultado = useInicio()
+  console.log(resultado)
   return (
     <Layout>
       <h1>Hola mundo</h1>
