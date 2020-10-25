09cb3249a03dec77ee687bd1a7f8173e121afb71
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-25 17:13:10 -0600

    Routing dinamico

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,27 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const urlSlug = require("url-slug")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const resultado = await graphql(`
+    query {
+      allStrapiPropiedades {
+        nodes {
+          nombre
+          id
+        }
+      }
+    }
+  `)
+
+  if (resultado.errors) {
+    reporter.panic("No hubo resultados", resultado.errors)
+  }
+
+  const propiedades = resultado.data.allStrapiPropiedades.nodes
+
+  propiedades.forEach(propiedad => {
+    actions.createPage({
+      path: urlSlug(propiedad.nombre),
+      component: require.resolve("./src/components/propiedades.jsx"),
+    })
+  })
+}
